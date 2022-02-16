a803bf8b14673eb935951b68bd9011ffe2548d3d
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2022-02-16 14:49:30 -0300

    Criação de função de menu hamburguer e configuração de responsividade

---
+++ matrix/Data/Repository/ServicoRepository.cs
@@ -0,0 +1,58 @@
+﻿using matrix.Dominio;
+using matrix.Dominio.Interfaces.Repository;
+using matrix.Models.Entidades;
+
+namespace matrix.Data.Repository
+{
+    public class ServicoRepository : Repository<Servicos>, IPostagemRepository
+    {
+        protected readonly cronosContext _context;
+        public ServicoRepository(cronosContext context) : base(context)
+        {
+        }
+        public void Atualizar(Postagem entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CriarNovo(Postagem entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(Postagem entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Servicos ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Servicos> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Salvar(Postagem entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        Postagem IRepository<Postagem>.ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Postagem> IRepository<Postagem>.ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- matrix/wwwroot/assets/js/main.js
+++ matrix/wwwroot/assets/js/main.js
@@ -35,7 +35,6 @@
     };
 
     window.onload = function () {
-        console.log(document.title);
         if (document.title !== "Home Page - matrix") {
             const ud_header = document.querySelector(".ud-header");
             const sticky = ud_header.offsetTop;
@@ -46,32 +45,45 @@
         }
     };
 
+    // teste
+    function testando() {
+        const navbarCollapse = document.querySelector("#navbarCollapse")
+        navbarCollapse.classList.add("hidden")
+    }
+    // fim teste
+
+
     // ===== responsive navbar
     let navbarToggler = document.querySelector("#navbarToggler");
     const navbarCollapse = document.querySelector("#navbarCollapse");
 
+
     navbarToggler.addEventListener("click", () => {
-        navbarToggler.classList.toggle("navbarTogglerActive");
-        navbarCollapse.classList.toggle("hidden");
+    navbarToggler.classList.toggle("navbarTogglerActive");
+    navbarCollapse.classList.toggle("hidden");
+  
     });
-
+    /*.querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")*/
     //===== close navbar-collapse when a  clicked
-    document
-        .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
-        .forEach((e) =>
-            e.addEventListener("click", () => {
-                navbarToggler.classList.remove("navbarTogglerActive");
-                navbarCollapse.classList.add("hidden");
-            })
-        );
+    //document
+    //    .querySelectorAll("#navbarCollapse ul li a")
+    //    .forEach((e) =>
+    //        e.addEventListener("click", () => {
+    //            navbarToggler.classList.remove("navbarTogglerActive");
+    //            navbarCollapse.classList.add("hidden");
+    //        })
+    //    );
 
-    // ===== Sub-menu
-    const submenuItems = document.querySelectorAll(".submenu-item");
-    submenuItems.forEach((el) => {
-        el.querySelector("a").addEventListener("click", () => {
-            el.querySelector(".submenu").classList.toggle("hidden");
-        });
-    });
+    /* ===== Sub-menu*/
+    //const submenuItems = document.querySelectorAll(".submenu-item");
+    //submenuItems.forEach((el) => {
+    //    el.querySelector("a").addEventListener("click", () => {
+    //        el.querySelector(".submenu").classList.toggle("hidden");
+    //    });
+    //});
+
+    
+    
 
     // ===== Faq accordion
     const faqs = document.querySelectorAll(".single-faq");
