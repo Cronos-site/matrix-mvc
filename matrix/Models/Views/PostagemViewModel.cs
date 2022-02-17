660aa92b7d4c397392db10bc2b8a3b2542883835
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2022-02-17 20:20:25 -0300

    tirando obrigatoriedade de postagemViewModel

--- matrix/Models/Views/PostagemViewModel.cs
+++ matrix/Models/Views/PostagemViewModel.cs
@@ -5,13 +5,13 @@ namespace matrix.Models.Views
 {
     public class PostagemViewModel
     {
-        public int idPost { get; set; }
+        public int? idPost { get; set; }
         [Required]
         public string? Descricao { get; set; }
         [Required]
         public string Titulo { get; set; }
         public string Resumo { get; set; }
-        public string Date { get; set; }
+        public string? Date { get; set; }
         public string LinkImagemPostagem { get; set; }
         public string NomePessoa { get; set; }
     }
